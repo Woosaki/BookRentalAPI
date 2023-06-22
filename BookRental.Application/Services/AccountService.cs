06dfef48e1c941f8a53367416343f5eb765c57c0
Woosaki <kamil.szczukowski@interia.pl>
2023-06-22 17:16:49 +0200

    Add authorization to swagger

--- BookRental.Application/Services/AccountService.cs
+++ BookRental.Application/Services/AccountService.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Security.Cryptography;
 using System.Text;
 
 namespace BookRental.Application.Services;
