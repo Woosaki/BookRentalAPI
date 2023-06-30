7b32b73f7d75494b77fcbe175bce6d017ec979e0
Woosaki <kamil.szczukowski@interia.pl>
2023-06-30 08:42:30 +0200

    refactor program.cs

--- BookRental.API/Extensions/AppExtensions.cs
+++ BookRental.API/Extensions/AppExtensions.cs
@@ -1,9 +1,17 @@
 ﻿using BookRental.API.Middlewares;
+using BookRental.Infrastructure;
 
 namespace BookRental.API.Extensions;
 
 public static class AppExtensions
 {
-	public static IApplicationBuilder UseErrorHandling(this IApplicationBuilder builder) =>
-		builder.UseMiddleware<ErrorHandlingMiddleware>();
+	public static IApplicationBuilder UseErrorHandling(this IApplicationBuilder app) =>
+		app.UseMiddleware<ErrorHandlingMiddleware>();
+
+	public static async Task SeedDatabaseAsync(this WebApplication app)
+	{
+		using var scope = app.Services.CreateScope();
+		var context = scope.ServiceProvider.GetService<BookRentalDbContext>();
+		await BookRentalDbContextSeed.SeedAsync(context);
+	}
 }
--- BookRental.API/Program.cs
+++ BookRental.API/Program.cs
@@ -1,102 +1,26 @@
 using BookRental.API.Extensions;
-using BookRental.Application.Dtos.UserDtos;
-using BookRental.Application.Interfaces;
-using BookRental.Application.Mapping;
-using BookRental.Application.Services;
-using BookRental.Application.Validators;
-using BookRental.Domain.Entities;
-using BookRental.Infrastructure;
-using FluentValidation;
-using FluentValidation.AspNetCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.EntityFrameworkCore;
-using NLog.Web;
-using System.Text.Json.Serialization;
-using System.Text;
-using Microsoft.OpenApi.Models;
-using BookRental.Application.Dtos.AuthorDtos;
-using BookRental.Application.Dtos.BookDtos;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<BookRentalDbContext>(options =>
-	options.UseSqlServer(
-		builder.Configuration.GetConnectionString("DefaultConnectionString"),
-		x => x.MigrationsAssembly("BookRental.Infrastructure")));
+builder.AddDbContext();
 
-builder.Logging.ClearProviders();
-builder.Logging.SetMinimumLevel(LogLevel.Trace);
-builder.Host.UseNLog();
-builder.Services.AddControllers()
-	.AddJsonOptions(options =>
-		options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.AddLogger();
 
-builder.Services.AddAuthentication(options =>
-{
-	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(cfg => 
-{
-	cfg.TokenValidationParameters = new TokenValidationParameters
-	{
-		ValidateIssuer = true,
-		ValidateAudience = true,
-		ValidateLifetime = true,
-		ValidateIssuerSigningKey = true,
-		ValidIssuer = builder.Configuration["Jwt:Issuer"],
-		ValidAudience = builder.Configuration["Jwt:Issuer"],
-		IssuerSigningKey = new SymmetricSecurityKey(
-			Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
-	};
-});
+builder.AddServices();
 
-builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
+builder.AddControllers();
 
-builder.Services.AddScoped<IAuthorService, AuthorService>();
-builder.Services.AddScoped<IBookService, BookService>();
-builder.Services.AddScoped<IAccountService, AccountService>();
-builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+builder.AddFluentValidation();
 
-builder.Services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
-builder.Services.AddScoped<IValidator<CreateAuthorDto>, CreateAuthorDtoValidator>();
-builder.Services.AddScoped<IValidator<CreateBookDto>, CreateBookDtoValidator>();
+builder.AddSwagger();
 
-builder.Services.AddAutoMapper(typeof(GeneralProfile).Assembly);
+builder.ConfigureAuthentication();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-	options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-	{
-		Description = "JWT Authorization header using the Bearer scheme",
-		Name = "Authorization",
-		In = ParameterLocation.Header,
-		Type = SecuritySchemeType.Http,
-		Scheme = "bearer"
-	});
-	options.AddSecurityRequirement(new OpenApiSecurityRequirement
-	{
-		{
-			new OpenApiSecurityScheme
-			{
-				Reference = new OpenApiReference
-				{
-					Type = ReferenceType.SecurityScheme,
-					Id = "Bearer"
-				}
-			},
-			Array.Empty<string>()
-		}
-	});
-});
 
 var app = builder.Build();
 
-using var scope = app.Services.CreateScope();
-var context = scope.ServiceProvider.GetService<BookRentalDbContext>();
-await BookRentalDbContextSeed.SeedAsync(context);
+await app.SeedDatabaseAsync();
 
 if (app.Environment.IsDevelopment())
 {
